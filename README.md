9f4f21e459626cded5497be2349b3e6bbc943954
luojh <bigtiger02@gmail.com>
2016-09-12 11:50:53 +0800

    init

---
+++ README.md
@@ -0,0 +1,31 @@
+# isometric for coding with ❤️
+- - - - -
+这是 isometric for github 的 coding 实现版，地址：[https://github.com/jasonlong/isometric-contributions.git](https://github.com/jasonlong/isometric-contributions.git)  
+![demo](http://7xrkla.com1.z0.glb.clouddn.com/isometric/iso-demo.png)
+目前只实现了谷歌插件。
+由于谷歌插件在2016年11月之后将不在支持系统用户的使用，故不再上传谷歌插件市场，请童鞋门自行下载安装体验。
+> 自 2016 年 11 月开始，您发布的任何新的封装应用或托管应用将仅限 Chrome 操作系统用户使用，而 Windows、Mac 或 Linux 用户将无法再使用这些应用。现有的应用将继续供所有主要平台的用户使用。  
+
+# 安装步骤
+- - - - --
+#### 1、git clone
+clone isometric 到本地
+```
+git clone https://git.coding.net/bigtiger02/isometric.git
+```  
+#### 2、 打开谷歌浏览器进入插件页面
+![步骤1](http://7xrkla.com1.z0.glb.clouddn.com/isometric/google-ext.png)
+#### 3、进入插件页面打开`开发者模式`
+![步骤2](http://7xrkla.com1.z0.glb.clouddn.com/isometric/google-ext2.png)
+####4、 加载已经下载的isometric插件
+![步骤3](http://7xrkla.com1.z0.glb.clouddn.com/isometric/google-ext3.png)
+####5、进入自己的coding 主页查看挂件
+![demo](http://7xrkla.com1.z0.glb.clouddn.com/isometric/iso-demo.png)
+
+#感谢
+- - - - -  
+* [jsonlong](https://github.com/jasonlong/isometric-contributions.git)  
+* [nosir](https://github.com/nosir/obelisk.js.git)  
+
+happy hack anyway ! :-)
+
---
+++ chrome/iso.js
@@ -0,0 +1,152 @@
+var Iso = (function(){
+  var totalContributions,totalContributionDateRange,
+    largestContribution,largestContributionTime,
+    longestContributions,longestContributionDateRange,
+    currentContributions,lastActiveTime;
+
+    function Iso(){
+      this._fetchData();
+      this._initUI();
+      this.renderIsometricChart();
+    }
+
+    Iso.prototype._fetchData = function(){
+      //总贡献度
+      $(".statistic-table tr:nth-child(2)").find("td:nth-child(1)").each(function(){
+        totalContributions = parseInt($(this).find(".count").html());
+        totalContributionDateRange = $(this).find(".duration").html();
+      });
+      //最长活跃度
+      $(".statistic-table tr:nth-child(2)").find("td:nth-child(2)").each(function(){
+        longestContributions = parseInt($(this).find(".count").html());
+        longestContributionDateRange = $(this).find(".duration").html();
+      });
+      //当前活跃度
+      $(".statistic-table tr:nth-child(2)").find("td:nth-child(3)").each(function(){
+        currentContributions = parseInt($(this).find(".count").html());
+        lastActiveTime = $(this).find(".duration").html();
+      });
+      //最大天提交量
+      $('.daily-activeness-graph').find("rect").each(function(){
+          var contribCount = parseInt($(this).attr('data-count'));
+          var contribDate = $(this).attr('data-date');
+          if(largestContribution){
+            if(contribCount > largestContribution){
+              largestContribution = contribCount;
+              largestContributionTime = contribDate;
+            }
+          }else{
+              largestContribution = contribCount;
+              largestContributionTime = contribDate;
+          }
+      });
+    }
+
+    Iso.prototype._initUI = function(){
+      $(".statistic-table").wrap('<div class="origin-tab-container"></div>');
+      $(".origin-tab-container").wrap('<div class="iso-container"></div>');
+      var isoHtml = '<div class="iso-tab-container">'
+                    +'<div class="iso-stats-top"><table>'
+                    +'<tr><td class="iso-td">活跃度<span class="iso-stats-cnt">'+totalContributions+'</span></td><td class="iso-meta-td"><span class="iso-stats-unit">过去一年</span><span class="iso-stats-date">'+totalContributionDateRange+'</span></td></tr>'
+                    +'<tr><td class="iso-td">最长活跃度<span class="iso-stats-cnt">'+largestContribution+'</span></td><td class="iso-meta-td"><span class="iso-stats-unit">天</span><span class="iso-stats-date">'+largestContributionTime+'</span></td></tr>'
+                    +'</table></div>'
+                    +'<div class="iso-stats-bottom"><table>'
+                    +'<tr><td class="iso-td">最长连续活跃<span class="iso-stats-cnt">'+longestContributions+'</span></td><td class="iso-meta-td"><span class="iso-stats-unit">天</span><span class="iso-stats-date">'+longestContributionDateRange+'</span></td></tr>'
+                    +'<tr><td class="iso-td">当前连续活跃<span class="iso-stats-cnt">'+currentContributions+'</span></td><td class="iso-meta-td"><span class="iso-stats-unit">天</span><span class="iso-stats-date">'+lastActiveTime+'</span></td></tr>'
+                    +'</table></div>'
+                    +'<canvas id="isometric-canvas" width="620px" height="400px"></canvas>'
+                    +'</div>';
+      $(".iso-container").prepend(isoHtml);
+      $(".iso-container").wrap('<div id="iso-coding-contributions"></div>');
+
+      var tabHtml = '<div class="iso-header">'
+                    +'<span class="title">Iso metric for coding with ❤️</span>'
+                    +'<span class="iso-toggle">'
+                    +'<a href="#" class="iso-toggle-option tooltipped squares"></a>'
+                    +'<a href="#" class="iso-toggle-option tooltipped cubes active"></a>'
+                    +'</span>'
+                    +'</div>';
+      $("#iso-coding-contributions").prepend(tabHtml);
+      $(".iso-toggle").find(".squares").click(function(){
+        $(".iso-toggle").find(".cubes").removeClass("active");
+        $(this).addClass("active");
+        $(".iso-tab-container").hide();
+        $(".origin-tab-container").show();
+      });
+      $(".iso-toggle").find(".cubes").click(function(){
+        $(".iso-toggle").find(".squares").removeClass("active");
+        $(this).addClass("active");
+        $(".iso-tab-container").show();
+        $(".origin-tab-container").hide();
+      });
+    }
+
+    Iso.prototype.renderIsometricChart = function(){
+      var GH_OFFSET, MAX_HEIGHT, SIZE, canvas, contribCount, pixelView, point, self;
+      SIZE = 10;
+      GH_OFFSET = 13;
+      MAX_HEIGHT = 80;
+      var maxCount = 20;
+      canvas = document.getElementById('isometric-canvas');
+      point = new obelisk.Point(97, 120);
+      pixelView = new obelisk.PixelView(canvas, point);
+      contribCount = null;
+      self = this;
+      $('.daily-activeness-graph').find("g").each(function(g) {
+        var x;
+        x = parseInt(((($(this)).attr('transform')).match(/(\d+)/))[0] / GH_OFFSET);
+        $(this).find('rect').each(function(r) {
+          var color, cube, cubeHeight, dimension, fill, p3d, y;
+          //r = ($(this)).get(0);
+          y = parseInt((($(this)).attr('y')) / GH_OFFSET);
+          fill = $(this).attr('fill');
+          contribCount = parseInt(($(this)).attr('data-count'));
+
+          cubeHeight = 3;
+          if (maxCount > 0) {
+            cubeHeight += parseInt(MAX_HEIGHT / maxCount * contribCount);
+          }
+          dimension = new obelisk.CubeDimension(SIZE, SIZE, cubeHeight);
+          color = self.getSquareColor(fill);
+          cube = new obelisk.Cube(dimension, color, false);
+          p3d = new obelisk.Point3D(SIZE * x, SIZE * y, 0);
+          pixelView.renderObject(cube, p3d);
+        });
+      });
+    }
+
+    Iso.prototype.getSquareColor = function(fill) {
+      var color;
+      return color = (function() {
+        var COLORS = [new obelisk.CubeColor().getByHorizontalColor(0xeeeeee),
+                  new obelisk.CubeColor().getByHorizontalColor(0xd6e685),
+                  new obelisk.CubeColor().getByHorizontalColor(0x8cc665),
+                  new obelisk.CubeColor().getByHorizontalColor(0x44a340),
+                  new obelisk.CubeColor().getByHorizontalColor(0x1e6923)];
+        switch (fill) {
+          case '#eeeeee':
+            return COLORS[0];
+          case '#d6e685':
+            return COLORS[1];
+          case '#8cc665':
+            return COLORS[2];
+          case '#44a340':
+            return COLORS[3];
+          case '#1e6923':
+            return COLORS[4];
+        }
+      })();
+    };
+
+    return Iso;
+})();
+
+$(function() {
+  //由于coding的图表为异步加载，处理异步加载偶尔延迟的情况
+  var time = setInterval(function(){
+    if($('.daily-activeness-graph').length > 0){
+      clearInterval(time);
+      new Iso();
+    }
+  },1000);
+});
